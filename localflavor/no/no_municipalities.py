6bdab65946207a7963c14a008faeef7fc26cd837
Håkon Osland <hakon.osland.ettestol@gmail.com>
2024-06-17 10:57:13 +0200

    Update no_municipalities.py
    The norwegian municipalities was changed in 2024: https://en.wikipedia.org/wiki/Counties_of_Norway . Update the list to reflect that

--- localflavor/no/no_municipalities.py
+++ localflavor/no/no_municipalities.py
@@ -1,25 +1,21 @@
 #: An alphabetical list of Norwegian municipalities (fylker) for
 #: use as `choices` in a formfield.
 MUNICIPALITY_CHOICES = (
+    ('agder', 'Agder'),
     ('akershus', 'Akershus'),
-    ('austagder', 'Aust-Agder'),
     ('buskerud', 'Buskerud'),
     ('finnmark', 'Finnmark'),
-    ('hedmark', 'Hedmark'),
-    ('hordaland', 'Hordaland'),
+    ('innlandet', 'Innlandet'),
     ('janmayen', 'Jan Mayen'),
     ('moreogromsdal', 'Møre og Romsdal'),
-    ('nordtrondelag', 'Nord-Trøndelag'),
+    ('trondelag', 'Trøndelag'),
     ('nordland', 'Nordland'),
-    ('oppland', 'Oppland'),
     ('oslo', 'Oslo'),
     ('rogaland', 'Rogaland'),
-    ('sognogfjordane', 'Sogn og Fjordane'),
     ('svalbard', 'Svalbard'),
-    ('sortrondelag', 'Sør-Trøndelag'),
     ('telemark', 'Telemark'),
     ('troms', 'Troms'),
-    ('vestagder', 'Vest-Agder'),
     ('vestfold', 'Vestfold'),
+    ('vestland', 'Vestland'),
     ('ostfold', 'Østfold')
 )
